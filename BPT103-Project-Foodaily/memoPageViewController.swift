8a34f722614d82c4bf5358ac57722a189d10df9d
Rabo <zusak96214287@gmail.com>
2017-09-14 19:56:53 +0800

    0914 Foodaily2 新建完成

---
+++ BPT103-Project-Foodaily/memoPageViewController.swift
@@ -0,0 +1,40 @@
+//
+//  memoPageViewController.swift
+//  BPT103-Project-Foodaily
+//
+//  Created by Rabo Lu on 2017/7/20.
+//  Copyright © 2017年 RaboLu. All rights reserved.
+//
+
+import UIKit
+
+class memoPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func goBackMainPage(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BPT103-Project-Foodaily/wantToEatViewController.swift
@@ -0,0 +1,43 @@
+//
+//  wantToEatViewController.swift
+//  BPT103-Project-Foodaily
+//
+//  Created by Rabo Lu on 2017/7/20.
+//  Copyright © 2017年 RaboLu. All rights reserved.
+//
+
+import UIKit
+
+class wantToEatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func goBackToMainPage(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func goBackToMainPage2(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
