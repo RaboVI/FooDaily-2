d2152b7e2cb65f45ea654b01c0384f45abd2ddeb
Rabo <zusak96214287@gmail.com>
2017-09-17 18:43:29 +0800

    0917 修改內容 //=================== 1.將資料夾改成中文 2.把Document Outline內的中文改為英文 3.將RootPageView跟PageViewController內的簡體字註解改為繁體中文

--- BPT103-Project-Foodaily/RootPageView.swift
+++ BPT103-Project-Foodaily/RootPageView.swift
@@ -16,19 +16,19 @@ class RootPageView: UIViewController,PageViewControllerDelegate {
     // 設立一個導覽點，會由PageViewController的Delegate傳回值來控制
     @IBOutlet weak var pageControl: UIPageControl!
     
-    // welcomeTitleLabel要顯示的文字
+    // welcomeTitleLabel上要顯示的文字
     let welcomeTitleText = ["壓力腦袋","壓力腦袋","壓力腦袋"]
     
-    // welcomeDetailLabel要顯示的文字
+    // welcomeDetailLabel上要顯示的文字
     let welcomeDetailText = [
         "原料回憶是以同時娛樂正確郵箱下載次數掌握簡介現金日誌，傳統想到介紹隨着後面好好戰爭上了達到您可以律師面對當中寂寞不以同時原料回憶",
-         "原料回憶是以同時娛樂正確郵箱下載次數掌握簡介現金日誌，傳統想到介紹隨着後面好好戰爭上了達到您可以律師面對當中寂寞不以同時原料回憶",
-         "原料回憶是以同時娛樂正確郵箱下載次數掌握簡介現金日誌，傳統想到介紹隨着後面好好戰爭上了達到您可以律師面對當中寂寞不以同時原料回憶"]
+        "原料回憶是以同時娛樂正確郵箱下載次數掌握簡介現金日誌，傳統想到介紹隨着後面好好戰爭上了達到您可以律師面對當中寂寞不以同時原料回憶",
+        "原料回憶是以同時娛樂正確郵箱下載次數掌握簡介現金日誌，傳統想到介紹隨着後面好好戰爭上了達到您可以律師面對當中寂寞不以同時原料回憶"]
 
-    //场景切换
+    // 準備一個頁面
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let pageViewController = segue.destination as? PageViewController {
-            //设置委托（当页面数量、索引改变时当前视图控制器能触发页控件的改变）
+            // 設置一個Delegate並把對象指定為RootPageView
             pageViewController.pageDelegate = self
         }
     }
@@ -37,7 +37,6 @@ class RootPageView: UIViewController,PageViewControllerDelegate {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
         welcomeDetailLabel.text = welcomeDetailText[0]
     }
 
@@ -50,6 +49,7 @@ class RootPageView: UIViewController,PageViewControllerDelegate {
     @IBAction func regeisterBTN(_ sender: Any) {
         
         
+        
     }
     
     // 進入會員登錄頁面
@@ -59,19 +59,17 @@ class RootPageView: UIViewController,PageViewControllerDelegate {
         
     }
     
-    // 計算點點的數量
+    // 計算導覽點的數量
     func pageViewController(_ pageViewController: PageViewController, didUpdatePageCount count: Int) {
         pageControl.numberOfPages = count
     }
     
-    // 設定點點的位置
+    // 設定導覽點的位置
     func pageViewController(_ pageViewController: PageViewController, didUpdatePageIndex index: Int) {
         pageControl.currentPage = index
         welcomeTitleLabel.text = welcomeTitleText[index]
         welcomeDetailLabel.text = welcomeDetailText[index]
     }
-
-
 
     /*
     // MARK: - Navigation
@@ -82,5 +80,4 @@ class RootPageView: UIViewController,PageViewControllerDelegate {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
