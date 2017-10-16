936f3411cd7c2d2c921898125b0269f34241f604
Huneshih <call0933@gmail.com>
2017-10-16 18:30:04 +0800

    本次處理之問題 ======== 1. 整理並清除不必要之檔案 2. 替換launchScreen照片
    修復方式 =============
    
    已知問題 =============
    
    新增功能 =============
    
    ---------------------------
    !! 請遵照上方格式新增備註

--- BPT103-Project-Foodaily/DiaryViewCustomSegue.swift
+++ BPT103-Project-Foodaily/DiaryViewCustomSegue.swift
@@ -10,24 +10,24 @@ import UIKit
 
 class DiaryViewCustomSegue: UIStoryboardSegue {
     
-    override func perform() {
-        var cellView = self.source.view as UIView!
-        var diaryView = self.destination.view as UIView!
-        
-        let screenSize = UIScreen.main.bounds.size
-        let cellFrame = cellView?.frame
-        
-        diaryView?.frame = CGRect(x: 50, y: 50, width: 0, height: 0)
-        
-        let window = UIApplication.shared.keyWindow
-        window?.insertSubview(diaryView!, aboveSubview: cellView!)
-        
-        UIView.animate(withDuration: 4, delay: 0, options: .curveEaseInOut, animations: {
-            diaryView?.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
-        }) { (_) in
-            print(cellFrame)
-            self.source.present(self.destination, animated: false, completion: nil)
-        }
-    }
+//    override func perform() {
+//        var cellView = self.source.view as UIView!
+//        var diaryView = self.destination.view as UIView!
+//        
+//        let screenSize = UIScreen.main.bounds.size
+//        let cellFrame = cellView?.frame
+//        
+//        diaryView?.frame = CGRect(x: 50, y: 50, width: 0, height: 0)
+//        
+//        let window = UIApplication.shared.keyWindow
+//        window?.insertSubview(diaryView!, aboveSubview: cellView!)
+//        
+//        UIView.animate(withDuration: 4, delay: 0, options: .curveEaseInOut, animations: {
+//            diaryView?.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
+//        }) { (_) in
+//            
+//            self.source.present(self.destination, animated: false, completion: nil)
+//        }
+//    }
 
 }
